b4445f593d854fba7e8f86f0a044ed17ad316c99
Ruben Verborgh <ruben@verborgh.org>
2018-07-04 16:05:52 +0200

    Remove test code.

--- src/hello.ts
+++
@@ -1,3 +0,0 @@
-export function hello(word: string = 'world'): string {
-  return `Hello ${word}!`;
-}
--- test/hello.ts
+++
@@ -1,5 +0,0 @@
-import { hello } from '../src/hello';
-
-it('greets Ann', () => {
-  expect(hello('Ann')).toBe('Hello Ann!');
-});
